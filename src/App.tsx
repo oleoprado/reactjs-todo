e96b850e7c38347f5203718f17ea559e15c32705
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-31 12:32:18 +0000

    feat: criado componente NewTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Header } from './components/Header';
 import { NewTask } from './components/NewTask';
 
+import styles from './App.module.css';
+
 import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <NewTask />
+      <div className={ styles.wrapper }>
+        <NewTask />
+        <div className={ styles.taskInProgress}>
+          <div className={ styles.createTask}>
+            <strong>Tarfeas Criadas</strong><span>0</span>
+          </div>
+          <div className={ styles.doneTask}>
+            <strong>Concluídas</strong><span>0</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,10 +1,19 @@
+import { PlusCircle } from 'phosphor-react';
+
 import styles from './NewTask.module.css';
 
 export function NewTask() {
   return(
     <form className={ styles.newTaskForm }>
-      <input type="text" />
-      <button>Criar</button>
+      <input type="text" placeholder="Adicione uma nova tarefa"/>
+      <button
+        type="submit">
+          <div className = {styles.containerButton}>
+            Criar
+            <PlusCircle size={16} weight="bold"/>
+
+          </div>
+        </button>
     </form>
   )
 }
